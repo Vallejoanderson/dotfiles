b9b407f9a0b80c5178741de750a05882f69f023c
Anderson Vallejo <vallejoanderson@gmail.com>
2023-02-17 12:19:54 -0500

    typo: small corrections

--- nvim/lua/user/plugins/dashboard.lua
+++ nvim/lua/user/plugins/dashboard.lua
@@ -11,10 +11,10 @@ db.custom_header = {
 }
 
 db.custom_center = {
-  { icon = '  ', desc = 'New file                       ', action = 'enew' },
-  { icon = '  ', shortcut = 'SPC f', desc = 'Find file                 ', action = 'Telescope find_files' },
-  { icon = '  ', shortcut = 'SPC h', desc = 'Recent files              ', action = 'Telescope oldfiles' },
-  { icon = '  ', shortcut = 'SPC g', desc = 'Find Word                 ', action = 'Telescope live_grep' },
+  { icon = '  ', desc = 'New file', action = 'enew' },
+  { icon = '  ', shortcut = 'SPC f', desc = 'Find file', action = 'Telescope find_files' },
+  { icon = '  ', shortcut = 'SPC h', desc = 'Recent files', action = 'Telescope oldfiles' },
+  { icon = '  ', shortcut = 'SPC g', desc = 'Find Word', action = 'Telescope live_grep' },
 }
 
 db.custom_footer = { '' }
